b1d7e03abf8f2923cd6dac69639b3451fd5336b1
Oleksii Prytchyn <2.2alpr@gmail.com>
2024-07-22 19:50:11 +0300

    Dice roll logic

--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from '../components/Button';
 import { Colors } from '../data/colors';
 import DiceImage from '../components/DiceImage';
 
 const MainScreen = () => {
+  const [diceNumber, setDiceNumber] = useState(0);
+
+  const rollTheDice = () => {
+    return Math.floor(Math.random() * 6) + 1;
+  };
+
   return (
     <View style={styles.mainContainer}>
       <Text style={styles.header}>Super Dice Game</Text>
       <View style={styles.imageContainer}>
-        <DiceImage imageNumber={0} />
+        <DiceImage imageNumber={diceNumber} />
       </View>
-      <Button title='Roll' isPrimary />
+      <Button
+        title='Roll'
+        isPrimary
+        onPressHandler={() => {
+          setDiceNumber(rollTheDice());
+        }}
+      />
       <View
         style={{
           height: 300,
